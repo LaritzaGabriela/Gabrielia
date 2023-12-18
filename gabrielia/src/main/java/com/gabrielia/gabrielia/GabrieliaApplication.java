5dfe671d20389f31ca3b633cb8178f88843924d3
LaritzaGabriela <146954226+LaritzaGabriela@users.noreply.github.com>
2023-12-18 12:29:24 +0100

    1 commit de inicio

---
+++ gabrielia/src/main/java/com/gabrielia/gabrielia/GabrieliaApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielia.gabrielia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabrieliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrieliaApplication.class, args);
+	}
+
+}
---
+++ gabrielia/src/test/java/com/gabrielia/gabrielia/GabrieliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielia.gabrielia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabrieliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
